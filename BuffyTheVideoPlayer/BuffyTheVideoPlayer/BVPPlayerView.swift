3536ce49babd88ed098b0b0c77e116236d6e86b5
asu <arsenyk.ustaris.dev@gmail.com>
2016-04-01 11:13:47 -0700

    Initial

---
+++ BuffyTheVideoPlayer/BuffyTheVideoPlayer/BVPPlayerView.swift
@@ -0,0 +1,26 @@
+//
+//  BVPPlayerView.swift
+//  BuffyTheVideoPlayer
+//
+//  Created by asu on 2016-04-01.
+//  Copyright © 2016 Singalong Software. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class BVPPlayerView: UIView
+{
+
+    var player = AVPlayer()
+    
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ BuffyTheVideoPlayer/BuffyTheVideoPlayer/BVPViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  BuffyTheVideoPlayer
+//
+//  Created by asu on 2016-04-01.
+//  Copyright © 2016 Singalong Software. All rights reserved.
+//
+
+import UIKit
+
+class BVPViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+                
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
