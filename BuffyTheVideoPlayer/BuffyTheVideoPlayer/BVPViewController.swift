7158ba89eb093b80128a5bba9dc40d87c5c32c93
asu <singalongsoftware@gmail.com>
2016-06-07 17:20:53 -0700

    Working data source and delegate code.

--- BuffyTheVideoPlayer/BuffyTheVideoPlayer/BVPViewController.swift
+++ BuffyTheVideoPlayer/BuffyTheVideoPlayer/BVPViewController.swift
@@ -9,13 +9,11 @@
 import UIKit
 import Photos
 
-class BVPViewController: UIViewController, UITableViewDataSource//, UITableViewDelegate
+class BVPViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
 {
 
     var media:[(index:Int, phAsset:PHAsset?, urlString:String?)] = [];
-    
-    @IBOutlet weak var playerView: BVPPlayerView!
-    
+        
     @IBOutlet weak var videoList: UITableView!
     
     override func viewDidLoad()
@@ -33,36 +31,32 @@ class BVPViewController: UIViewController, UITableViewDataSource//, UITableViewD
         
         self.media.removeAll()
         
-        self.media.append((0, nil, "/A"))
-        self.media.append((1, nil, "/B"))
-        self.media.append((2, nil, "/C"))
-        
-//        videos.enumerateObjectsUsingBlock(
-//        { (enumeratedObject, index, stopPointer) in
-//            let video = enumeratedObject as! PHAsset
-//            let imageManager = PHImageManager.defaultManager()
-//            
-//            
-//            imageManager.requestAVAssetForVideo(video, options: nil, resultHandler:
-//            { (avAsset:AVAsset?, audoMix:AVAudioMix?, info:[NSObject : AnyObject]?) in
-//                
-//                let avUrlAsset = avAsset as? AVURLAsset
-//                print("\(index) \(avUrlAsset?.URL.absoluteString)")
-//                
-//                self.media.append((index, nil, avUrlAsset?.URL.absoluteString))
-//
-//                if self.media.count == videos.count
-//                {
-//                    dispatch_async(dispatch_get_main_queue(),{
-//                        self.videoList.reloadData()
-//                    })
-//                
-//                    print("reload")
-//                    
-//                }
-//            })
-//            
-//        })
+        videos.enumerateObjectsUsingBlock(
+        { (enumeratedObject, index, stopPointer) in
+            let video = enumeratedObject as! PHAsset
+            let imageManager = PHImageManager.defaultManager()
+            
+            
+            imageManager.requestAVAssetForVideo(video, options: nil, resultHandler:
+            { (avAsset:AVAsset?, audoMix:AVAudioMix?, info:[NSObject : AnyObject]?) in
+                
+                let avUrlAsset = avAsset as? AVURLAsset
+                print("\(index) \(avUrlAsset?.URL.absoluteString)")
+                
+                self.media.append((index, nil, avUrlAsset?.URL.absoluteString))
+
+                if self.media.count == videos.count
+                {
+                    dispatch_async(dispatch_get_main_queue(),{
+                        self.videoList.reloadData()
+                    })
+                
+                    print("reload")
+                    
+                }
+            })
+            
+        })
         
 
     }
@@ -95,18 +89,12 @@ class BVPViewController: UIViewController, UITableViewDataSource//, UITableViewD
     
     // MARK: UITableViewDelegate
     
-//    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
-//    {
-//        guard let videoAddress = self.media[indexPath.row].urlString else {return}
-//        print("Play \(videoAddress)")
-//    }
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
+    {
+        guard let videoAddress = self.media[indexPath.row].urlString else {return}
+        print("Play \(videoAddress)")
+    }
     
-    // MARK: Orientation
-    
-//    override func didRotateFromInterfaceOrientation(fromInterfaceOrientation: UIInterfaceOrientation)
-//    {
-//        loadVideoList()
-//    }
     
 }
 
